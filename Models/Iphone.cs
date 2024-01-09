17bcbe70e8c44da8dabd09f3560dd9fa001c491e
Rhoney Seabra <rhoneyholanda@gmail.com>
2024-01-09 17:35:44 -0300

    Bem, meio que buguei. Mas vou reiterar depois.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,7 +3,15 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+         
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
-        public override void InstalarAplicativo(string nomeApp);
+        public override void InstalarAplicativo(string Telegram)
+        {
+            Console.WriteLine($"Instalando o aplicativo {InstalarAplicativo} no Iphone.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "12121312", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "13131213", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
